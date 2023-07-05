44f4a97b9557eddcfeea959be237d451b607e8c8
Guillaume ARM <guillaume.arm@payfit.com>
2023-07-05 16:28:50 +0200

    feat: map operator correction

--- src/operators/map.ts
+++ src/operators/map.ts
@@ -2,7 +2,22 @@ import { Observable } from '../Observable';
 import { OperatorFunction } from './types';
 
 export function map<T, R>(project: (value: T) => R): OperatorFunction<T, R> {
-  void Observable;
-  void project;
-  throw new Error('TODO');
+  return source => {
+    return new Observable(observer => {
+      const next = (value: T) => {
+        try {
+          const result = project(value);
+          observer.next(result);
+        } catch (e) {
+          observer.error(e);
+        }
+      };
+
+      return source.subscribe({
+        next,
+        error: err => observer.error(err),
+        complete: () => observer.complete(),
+      });
+    });
+  };
 }
